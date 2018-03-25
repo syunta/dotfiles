3e1e361a359400e3f156b2e604ec5061a09e74b8
syunta <kitagawa@timedia.co.jp>
2018-03-25 14:44:47 +0900

    Improve create_symlink

--- create_symlink.sh
+++ create_symlink.sh
@@ -2,15 +2,23 @@
 
 repo=$1
 
-ln -s "$repo/.gitconfig"        ~/.gitconfig
+rm ~/.gitconfig
+ln -s "$repo/.gitconfig" ~/.gitconfig
+rm ~/.gitignore_global
 ln -s "$repo/.gitignore_global" ~/.gitignore_global
-ln -s "$repo/.tmux.conf"        ~/.tmux.conf
-ln -s "$repo/.vimrc"            ~/.vimrc
-ln -s "$repo/.zprofile"         ~/.zprofile
-ln -s "$repo/.zshrc"            ~/.zshrc
-ln -s "$repo/.ghci"             ~/.ghci
+rm ~/.tmux.conf
+ln -s "$repo/.tmux.conf" ~/.tmux.conf
+rm ~/.vimrc
+ln -s "$repo/.vimrc" ~/.vimrc
+rm ~/.zprofile
+ln -s "$repo/.zprofile" ~/.zprofile
+rm ~/.zshrc
+ln -s "$repo/.zshrc" ~/.zshrc
+rm ~/.ghci
+ln -s "$repo/.ghci" ~/.ghci
 
 if [ ! -d ~/.config/nvim ]; then
   mkdir -p ~/.config/nvim
 fi
+rm ~/.config/nvim/init.vim
 ln -s "$repo/init.vim" ~/.config/nvim/init.vim
